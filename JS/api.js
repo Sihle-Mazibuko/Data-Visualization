6cc08c56e4ac94407ceb099d3aadd50fe13d0558
Sihle-Mazibuko <sihlemazii@gmail.com>
2023-09-06 22:21:13 +0200

    Homepage redo

--- JS/api.js
+++ JS/api.js
@@ -1,16 +1,19 @@
-// async function getWeather() {
-//   let apiKey = "8yTheQIGpatO25KHaczru6p8jd3Z2HlAU0InUaKD";
+const apiKey = "8yTheQIGpatO25KHaczru6p8jd3Z2HlAU0InUaKD";
 
-//   let response = await fetch(`https://eonet.gsfc.nasa.gov/api/v2.1/events`, {
-//     apiKey,
-//   });
-//   console.log(response);
-//   let data = await response.json();
-//   console.log(data);
-//   useData(data);
-// }
+async function RequestImage() {
+  let response = await fetch(
+    `https://api.nasa.gov/planetary/apod?api_key=${apiKey}`
+  );
+  let data = await response.json();
+  console.log(data);
+  useAPIData(data);
+}
 
-// function useData(data) {
-//   document.querySelector("#about-site").innerHTML += data.title;
-// }
-// getWeather();
+function useAPIData(data) {
+  document.querySelector("#image-title").innerHTML = data.title;
+  document.querySelector("#image").innerHTML += `<img src="${data.url}"`;
+  document.querySelector("#img-description").innerHTML = data.explanation;
+  document.querySelector("#img-date").innerHTML = data.date;
+}
+
+RequestImage();
